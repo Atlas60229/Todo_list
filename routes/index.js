31ea6f714d8627682e090fdd05747dab6bc5b034
Atlas60229 <b1010989126@tmu.edu.tw>
2023-02-07 13:08:07 +0800

    user model

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // use 
-router.use('/', home) // 將網址結構符合 / 字串的 request 導向 home 模組 
+router.use('/', home)       // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/todos', todos) // 將網址結構符合 /todos 字串的 request 導向 todos 模組 
+router.use('/users', users)  // 將網址結構符合 /users 字串的 request 導向 todos 模組 
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/register',(req,res)=>{
+    
+})
+
+router.post('/register',(req,res)=>{
+    
+})
+
+
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+router.post('/login',(req,res)=>{
+    
+})
+
+router.get('/logout',(req,res)=>{
+    
+})
+
+module.exports = router
