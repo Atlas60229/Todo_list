508d8abecb37dd12f3be5fc7552b28d9919a747f
Atlas60229 <b1010989126@tmu.edu.tw>
2023-02-08 09:52:41 +0800

    feat expression-session install

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 3000 // PORT會由heroku自動提供到環境
 const exphbs = require('express-handlebars')
@@ -9,6 +10,11 @@ const routes = require('./routes')
 require('./config/mongoose')  //執行app.js時會一併執行mongoose.js
 
 // use
+app.use(session({               //  需在較前端載入
+    secret: "ThisIsMySecret",
+    resave: false,              //  每次使用者發出request時，是否即使 Session 沒做變動，強制重新儲存進 Store。
+    saveUninitialized: true     //  是否強制將未初始化的 Session 儲存至 Store。（新產生的 Session）
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
