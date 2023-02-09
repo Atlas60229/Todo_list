25522c01a700750c569ef7bd49e21849b3ee4413
Atlas60229 <b1010989126@tmu.edu.tw>
2023-02-09 15:22:19 +0800

    add authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const {authenticator} = require('../middleware/auth')
 
-// use 
-router.use('/', home)       // 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/todos', todos) // 將網址結構符合 /todos 字串的 request 導向 todos 模組 
+// use (照順序比對)
+
+router.use('/todos',authenticator, todos) // 將網址結構符合 /todos 字串的 request 導向 todos 模組 
 router.use('/users', users)  // 將網址結構符合 /users 字串的 request 導向 todos 模組 
+router.use('/', authenticator, home)       // 將網址結構符合 / 字串的 request 導向 home 模組 
 
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -42,7 +42,13 @@ router.post('/login', passport.authenticate('local', {
 }))
 
 router.get('/logout', (req, res) => {
-
+    req.logout( err => {
+        if (err){
+            return next(err)
+        }
+        res.redirect('/users/login')
+    })
+    
 })
 
 module.exports = router
