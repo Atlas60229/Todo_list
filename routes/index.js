45282d62b2f38293a32ab39960c36a7a212aa949
Atlas60229 <b1010989126@tmu.edu.tw>
2023-02-12 17:05:11 +0800

    test git

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,12 @@ const todos = require('./modules/todos')
 const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth')
 const auth = require('./modules/auth')
-// use (照順序比對)
 
+// use (照順序比對)
 router.use('/todos', authenticator, todos) // 將網址結構符合 /todos 字串的 request 導向 todos 模組 
 router.use('/users', users)  // 將網址結構符合 /users 字串的 request 導向 todos 模組 
 router.use('/auth', auth)
 router.use('/', authenticator, home)       // 將網址結構符合 / 字串的 request 導向 home 模組 
+console.log('test')
 
 module.exports = router
